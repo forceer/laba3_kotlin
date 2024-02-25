aa7ba29ccfa8c2d69ebd1b1ef956fbe8c9c85f3a
forceer <anton.klimchuk.2004@gmail.com>
2024-02-25 13:45:23 +0300

    final preparation

--- app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/ScrollingScreen.kt
@@ -3,13 +3,17 @@ package com.raywenderlich.android.lab1.screens
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.raywenderlich.android.lab1.R
 import com.raywenderlich.android.lab1.router.BackButtonHandler
 import com.raywenderlich.android.lab1.router.FundamentalsRouter
 import com.raywenderlich.android.lab1.router.Screen
@@ -27,7 +31,10 @@ fun ScrollingScreen(){
 
 @Composable
 fun MyScrollingScreen(){
+    Column() {
 
+
+    }
 }
 
 @Composable
